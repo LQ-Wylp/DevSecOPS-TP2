332c470c4db4bb9ba23604893e5a945cd33ffe11
LEULIET Quentin <leuliet.quentin.online@gmail.com>
2024-03-05 11:13:02 +0100

    feat(interfaceCadeau):Ajout dependance twig & form cadeau & page cadeau (Affiche la liste des cadeaux)

---
+++ src/Controller/CadeauController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Cadeau;
+use App\Form\CadeauType;
+use App\Repository\CadeauRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class CadeauController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    private CadeauRepository $cadeauRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, CadeauRepository $cadeauRepository)
+    {
+        $this->entityManager = $entityManager;
+        $this->cadeauRepository = $cadeauRepository;
+    }
+
+    #[Route('/cadeau', name: 'app_cadeau')]
+    public function index(): Response
+    {
+        $cadeaux = $this->cadeauRepository->findAll();
+        return $this->render('cadeau/index.html.twig', [
+            'cadeaux' => $cadeaux
+        ]);
+    }
+
+    #[Route('/cadeau/new', name: 'app_cadeau_new')]
+    public function new(): Response
+    {
+        $cadeau = new Cadeau();
+
+        $form = $this->createForm(CadeauType::class, $cadeau);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->entityManager->persist($cadeau);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_cadeau');
+        }
+
+        return $this->render('cadeau/new.html.twig',[
+            'form' => $form,
+        ]);
+    }
+}
